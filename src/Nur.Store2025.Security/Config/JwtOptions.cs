500a98434a99811870cb2815fbeefd8ed5fa8f0d
Jose Carlos Gutierrez <joseco48@gmail.com>
2025-05-03 06:47:10 -0400

    Configuracion de seguridad compartida para los microservicios

---
+++ src/Nur.Store2025.Security/Config/JwtOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace Nur.Store2025.Security.Config;
+
+public class JwtOptions
+{
+    public string SecretKey { get; set; }
+    public bool ValidateIssuer { get; set; }
+    public bool ValidateAudience { get; set; }
+    public string ValidIssuer { get; set; }
+    public string ValidAudience { get; set; }
+    public bool ValidateLifetime { get; set; }
+    public long Lifetime { get; set; }
+
+    public JwtOptions()
+    {
+        SecretKey = "";
+        ValidIssuer = "";
+        ValidAudience = "";
+    }
+}
---
+++ src/Nur.Store2025.Security/DependencyInjection.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Nur.Store2025.Security.Config;
+using System.Text;
+
+namespace Nur.Store2025.Security;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSecurity(this IServiceCollection services, JwtOptions jwtoptions, IEnumerable<string> permissions)
+    {
+        services.AddAuthorization(config =>
+        {
+            var defaultAuthBuilder = new AuthorizationPolicyBuilder();
+            var defaultAuthPolicy = defaultAuthBuilder
+                .RequireAuthenticatedUser()
+                .Build();
+
+
+            config.DefaultPolicy = defaultAuthPolicy;
+
+            permissions.ToList().ForEach(permission =>
+            {
+                config.AddPolicy(permission, policy =>
+                {
+                    policy.RequireAssertion(context => 
+                        context.User.HasClaim( 
+                            c => c.Type == "scope" && c.Value.Split(" ").Contains(permission)
+                        )
+                    );
+                });
+            });
+        });
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer("Bearer", jwtOptions =>
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtoptions.SecretKey));
+            jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = jwtoptions.ValidateIssuer,
+                ValidateAudience = jwtoptions.ValidateAudience,
+                ValidIssuer = jwtoptions.ValidIssuer,
+                ValidAudience = jwtoptions.ValidAudience
+            };
+        });
+
+
+        return services;
+    }
+}
